326a02c32d32e89767fc1a7cd805475e2ab1e130
Antonio Rufino <devs.toni@gmail.com>
2023-03-04 21:46:02 +0100

    finish search styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,11 @@
 import { faMagnifyingGlass, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 import { useUI } from '../context/UIContext';
+import { Connection } from '../helpers/HTTP_Connection';
+import { productsUrl } from '../config.js';
+import uuid from 'react-uuid';
 
 const Search = () => {
 
@@ -10,24 +13,36 @@ const Search = () => {
   const { state: ui_state, dispatch: ui_dispatch } = handleUi();
 
   const navigate = useNavigate();
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState('');
+
+  const [tempResults, setTempResults] = useState([]);
+  const inputSearch = useRef(null);
 
   const handleChange = ({ target }) => {
     const { value } = target;
     setInput(value)
+    const { get } = Connection();
+    if (input.length > 0) {
+      get(`${productsUrl}/search/${input}`)
+        .then(data => setTempResults(data))
+        .catch(error => console.error(error));
+    } else {
+      setTempResults([]);
+    }
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate(`/product-category/bycicles/search/${input}`);
     setInput('');
-    ui_dispatch({type: UI_ACTIONS.CLOSE_SEARCH});
+    ui_dispatch({ type: UI_ACTIONS.CLOSE_SEARCH });
   }
-  
+
   useEffect(() => {
     setInput('');
+    setTempResults([]);
+    inputSearch.current.focus();
   }, [ui_state.searchIsOpen])
-
 
   return (
     <div className={`search ${ui_state.searchIsOpen ? "active" : ""}`}>
@@ -36,12 +51,27 @@ const Search = () => {
           <FontAwesomeIcon onClick={handleSubmit} icon={faMagnifyingGlass} />
         </div>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={input} onChange={handleChange} />
+          <input type="text" value={input} onChange={handleChange} ref={inputSearch} />
         </form>
         <div className='search__container--close'>
           <FontAwesomeIcon icon={faXmark} onClick={() => ui_dispatch({ type: UI_ACTIONS.CLOSE_SEARCH })} />
         </div>
       </div>
+      <ul className='search__results'>
+        {
+          input.length > 0
+          &&
+          tempResults.map((res, ind) => {
+            if (ind < 10) {
+              return (
+                <li key={uuid()} className="search__results--result" onClick={() => { ui_dispatch({ type: UI_ACTIONS.CLOSE_SEARCH }) }}>
+                  <Link to={`/product/options/${res.type}/${res.id}`}>{res.name}</Link>
+                </li>
+              )
+            }
+          })
+        }
+      </ul>
     </div>
   )
 }
