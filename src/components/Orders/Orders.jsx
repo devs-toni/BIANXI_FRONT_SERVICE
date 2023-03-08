ae23bbff706d796669db77c0e530d3017fb5d20d
Antonio Rufino <devs.toni@gmail.com>
2023-03-08 09:25:13 +0100

    create orders section

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import uuid from "react-uuid";
+import { ordersUrl } from "../../config";
+import { useLanguage } from "../../context/GlobalContext";
+import { useUser } from "../../context/UserContext";
+import { http } from "../../helpers/http";
+import { formatNumberES } from '../../helpers/utils';
 
 const Orders = () => {
+
+  const { handleUser } = useUser();
+  const { state: user_state } = handleUser();
+
+  const { text } = useLanguage();
+
+  const [orders, setOrders] = useState([]);
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const getOrders = async (userId) => {
+      await http()
+        .get(`${ordersUrl}/get/all/${userId}`)
+        .then(ordersBackend => {
+          setOrders([...ordersBackend]);
+        });
+    }
+    const getProducts = async () => {
+      orders.filter(async order => {
+        await http()
+          .get(`${ordersUrl}/get/products/${order.id}`)
+          .then(prod => {
+            setProducts([...products, prod = {
+              order: order.id,
+              products: prod
+            }])
+          })
+      })
+    }
+    getOrders(user_state.id);
+    getProducts();
+
+  }, []);
+
+
+
+
   return (
     <div className='orders'>
-      
+      <div className="orders__titles">
+        <p className="orders__titles--key">#</p>
+        <p className="orders__titles--key">{text.payment.address}</p>
+        <p className="orders__titles--key">{text.payment.products}</p>
+        <p className="orders__titles--key">{text.payment.total}</p>
+      </div>
+      {
+        (orders)
+        &&
+        orders.map(({ id, address, price }) => {
+          return (
+            <div className="orders__order" key={uuid()}>
+              <p className="orders__order--val">{id}</p>
+              <p className="orders__order--val">{address}</p>
+              <div className="orders__order--product">
+                {
+                  products.map(({ products, order }) => {
+                    console.log(products)
+                    if (order === id) {
+                      products.map(({ name }) => {
+                        return (
+                          <React.Fragment key={uuid()}>
+                            <p>{name}</p>
+                          </React.Fragment>
+                        )
+                      })
+                    }
+                  })
+                }
+              </div>
+              <p className="orders__order--val">{formatNumberES(price)} €</p>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -14,15 +14,17 @@ export const UserProvider = ({ children }) => {
     isLogged: loginStorage ? true : false,
     error: '',
     username: loginStorage ? loginStorage.username : '',
-    id: loginStorage ? loginStorage.id : 0
+    id: loginStorage ? loginStorage.id : 0,
+    orders: []
   }
 
   const USER_ACTIONS = {
+
     LOGIN_SUCCESS: "LOGIN_SUCCESS",
     LOGIN_ERROR: "LOGIN_ERROR",
     HANDLE_LIKE: "HANDLE_LIKE",
     RESET_ERROR: "RESET_ERROR",
-    LOGOUT: "LOGOUT",
+    LOGOUT: "LOGOUT"
   }
 
   const reducer = (state, action) => {
@@ -52,6 +54,7 @@ export const UserProvider = ({ children }) => {
           username: action.payload.username,
           id: action.payload.id
         }
+
       case USER_ACTIONS.LOGOUT:
         localStorage.removeItem('LOGIN_SUCCESS')
         return {
