4d5b3b1fd2e6f0c6fce844cda9546963898e4718
Antonio Rufino <devs.toni@gmail.com>
2023-03-12 18:33:52 +0100

    Delete completely ebike

--- src/components/Home/Section.jsx
+++ src/components/Home/Section.jsx
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types';
 
 const Section = ({ name, icon, path }) => {
   return (
-    <Link className='sections__link' to={path ? `product-category/bycicles/${path}` : ''}>
-      <div className='sections__section'>
+    <Link className={`${path ? "" : "disable"} sections__link`} to={path ? `product-category/bycicles/${path}` : ''}>
+      <div className={`${path ? "" : "disable"} sections__section`}>
         <FontAwesomeIcon className='sections__section--image' icon={icon} />
         <p className='sections__section--name'>{name.toLowerCase()}</p>
       </div>
