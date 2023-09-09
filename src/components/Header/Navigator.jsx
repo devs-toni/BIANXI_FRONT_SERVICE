9a1a9e616f11658fc2d0bca5c070f2d19e49f05f
Antonio Rufino <devs.toni@gmail.com>
2023-09-09 19:30:12 +0200

    feature: add serious langauages

--- src/components/Header/Navigator.jsx
+++ src/components/Header/Navigator.jsx
@@ -2,30 +2,32 @@ import React from 'react'
 import { useLanguage } from '../../context/GlobalContext';
 import { Dropdown } from '../index';
 import PropTypes from 'prop-types'
-import es from '../../assets/images/lang/es.png';
-import en from '../../assets/images/lang/en.png';
-import it from '../../assets/images/lang/it.png';
+
+const langs = [
+  {
+    text: "ES"
+  },
+  {
+    text: "EN"
+  },
+  {
+    text: "IT"
+  }
+];
 
 const Navigator = ({ parentStyles, items, handler }) => {
 
   const { text } = useLanguage();
   const { language, handleLanguage } = useLanguage();
 
-  const languageStyle = (lang) => language === lang ? "active" : '';
-
   return (
     <nav className={parentStyles}>
-      <div className="languages">
-        <div className={`navbar__options--lang ${languageStyle('es') ? "active" : ''}`}>
-          <img src={es} alt="es" name="es" onClick={handleLanguage} />
-        </div>
-        <div className={`navbar__options--lang ${languageStyle('en') ? "active" : ''}`}>
-          <img src={en} alt="en" className='w' name="en" onClick={handleLanguage} />
-        </div>
-        <div className={`navbar__options--lang ${languageStyle('it') ? "active" : ''}`}>
-          <img src={it} alt="it" className="w" name="it" onClick={handleLanguage} />
-        </div>
-      </div>
+      <Dropdown
+        items={langs}
+        dropdownTitle={language.toUpperCase()}
+        handler={handleLanguage}
+        style={{ minWidth: "100px" }}
+      />
       <Dropdown
         items={items}
         dropdownTitle={text.header.bycicles}
